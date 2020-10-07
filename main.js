426e69868eec28f3845eaef06e897ac14fab4a4a
Mukhtar <mukhtartanimugarba@gmail.com>
2020-10-07 17:09:22 +0100

    Files updated

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
 import { square } from './models.js';
 
+function toPoint(values) {
+    return {
+        x: values[0],
+        y: values[1],
+        z: values[2]
+    };
+}
+
+function toPolygon(shape) {
+    return shape.map(toPoint);
+}
+
+function toMesh(shape) {
+    return square.map(toPolygon);
+}
+
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-console.log(square);
+const mesh = toMesh(square);
+
+function drawPolygon(polygon, context) {
+    context.beginPath();
+
+    const first = polygon[0];
+    context.moveTo(first.x, first.y);
+}
+
+mesh.forEach(polygon => {
+
+});
+
+
+console.log(mesh);
