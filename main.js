963364074b017fca795e20e9b09af7d7dcfeb7e5
Mukhtar <mukhtartanimugarba@gmail.com>
2020-10-19 07:08:05 +0100

    File addition and update

--- main.js
+++ main.js
@@ -1,28 +1,12 @@
 import {square, doubleSquare, cube} from './models.js';
 import {drawPolygon} from './draw.js';
 import {Camera} from './camera.js';
-
-function toPoint(values) {
-    return {
-        x: values[0],
-        y: values[1],
-        z: values[2]
-    };
-}
-
-function toPolygon(shape) {
-    return shape.map(toPoint);
-}
-
-function toMesh(shape) {
-    return shape.map(toPolygon);
-}
+import {toMesh} from './mesh.js';
 
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 const mesh = toMesh(cube);
-const position= {x: 0, y: 0, z: 0};
 
 const camera = new Camera();
 camera.pos.z = 200;
@@ -30,18 +14,12 @@ camera.zoom = 12;
 
 context.strokeStyle = "#fff";
 
-function offset(point, position) {
-    point.x += position.x;
-    point.y += position.y;
-    point.z += position.z;
-}
-
 function drawMesh(mesh) {
-    mesh.forEach(polygon => {
+    mesh.polygons.forEach(polygon => {
         const projectedPolygon = polygon.map(point => ({...point}));
 
         projectedPolygon.forEach(point => {
-            offset(point, position);
+            mesh.transform(point);
            camera.transform(point);
         });
     
@@ -51,8 +29,9 @@ function drawMesh(mesh) {
 
 function animate(time) {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    position.x =Math.sin(time / 300) * 80;
-    position.y =Math.sin(time / 1000) * 80;
+    mesh.rotation.y += 0.01;
+    mesh.position.x = Math.sin(time / 300) * 80;
+    mesh.position.y = Math.sin(time / 1000) * 80;
     camera.pos.z += 0.1;
     drawMesh(mesh);
     requestAnimationFrame(animate);
