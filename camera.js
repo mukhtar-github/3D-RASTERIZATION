dcd18c85bcf12cb76509b6e2f5c7c60257f60746
Mukhtar <mukhtartanimugarba@gmail.com>
2020-10-19 05:50:31 +0100

    Files update

--- camera.js
+++ camera.js
@@ -1,15 +1,3 @@
-function perspective(point, distance) {
-    const fov = point.z + distance;
-    point.x /= fov;
-    point.y /= fov;
-}
-
-function zoom(point, factor) {
-    const scale = Math.pow(factor, 2);
-    point.x *= scale;
-    point.y *= scale;
-}
-
 export class Camera {
     constructor() {
         this.pos = {z: 100};
@@ -20,4 +8,16 @@ export class Camera {
         perspective(point, this.pos.z);
         zoom(point, this.zoom);
     }
+}
+
+function perspective(point, distance) {
+    const fov = point.z + distance;
+    point.x /= fov;
+    point.y /= fov;
+}
+
+function zoom(point, factor) {
+    const scale = Math.pow(factor, 2);
+    point.x *= scale;
+    point.y *= scale;
 }
